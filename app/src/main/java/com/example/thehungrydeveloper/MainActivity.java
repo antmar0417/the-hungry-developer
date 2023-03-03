0dee2c64509375108d6db3979458eca105f299d8
antmar0417 <tonyboy@live.se>
2023-03-03 15:39:55 +0100

    id selector

--- app/src/main/java/com/example/thehungrydeveloper/MainActivity.java
+++ app/src/main/java/com/example/thehungrydeveloper/MainActivity.java
@@ -1,14 +1,21 @@
 package com.example.thehungrydeveloper;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    CardView starterCard;
+    CardView mainsCard;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        starterCard = findViewById(R.id.card_view_starters);
+        mainsCard = findViewById(R.id.card_view_mains);
     }
 }
