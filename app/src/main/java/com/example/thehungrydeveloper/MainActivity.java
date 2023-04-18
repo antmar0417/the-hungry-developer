d0479da045d864cc4c0990fef912e2dd87e75901
anton-marinov <anton.marinov@polestar.com>
2023-04-18 18:19:47 +0200

    created starters adapter

--- app/src/main/java/com/example/thehungrydeveloper/MainActivity.java
+++ app/src/main/java/com/example/thehungrydeveloper/MainActivity.java
@@ -3,7 +3,10 @@ package com.example.thehungrydeveloper;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,5 +20,13 @@ public class MainActivity extends AppCompatActivity {
 
         starterCard = findViewById(R.id.card_view_starters);
         mainsCard = findViewById(R.id.card_view_mains);
+
+        starterCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent startersActivityIntent = new Intent(MainActivity.this, StartersActivity.class);
+                startActivity(startersActivityIntent);
+            }
+        });
     }
 }
