d1a7270f76583f3aecefecd7b9ac510cde628449
anton-marinov <anton.marinov@polestar.com>
2024-05-12 20:20:39 +0200

    created a class dish

---
+++ app/src/main/java/com/example/thehungrydeveloper/Dish.java
@@ -0,0 +1,24 @@
+package com.example.thehungrydeveloper;
+
+import androidx.annotation.NonNull;
+
+import java.util.concurrent.ScheduledExecutorService;
+
+public class Dish {
+
+    String title;
+    String description;
+    int price;
+
+    Dish(String title, String description, int price) {
+        this.title = title;
+        this.description = description;
+        this.price = price;
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return title;
+    }
+}
